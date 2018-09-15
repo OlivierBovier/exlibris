6bad95fc1aecc67231420f334b9d473ed35e5931
Olivier Bovier <olivier.bovier@free.fr>
2018-09-15 10:42:01 +0200

    admin et front auteurs

--- src/Entity/Auteurs.php
+++ src/Entity/Auteurs.php
@@ -5,9 +5,12 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\AuteursRepository")
+ * @Vich\Uploadable
  */
 class Auteurs
 {
@@ -34,14 +37,27 @@ class Auteurs
     private $biographie_auteur;
 
     /**
-     * @ORM\Column(type="string", length=255, nullable=true)
-     */
-    private $photo_auteur;
-
-    /**
      * @ORM\OneToMany(targetEntity="App\Entity\Livres", mappedBy="auteur", orphanRemoval=true)
      */
     private $livres;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @var string
+     */
+    private $image;
+
+    /**
+     * @Vich\UploadableField(mapping="auteurs_images", fileNameProperty="image")
+     * @var File
+     */
+    private $imageFile;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     * @var \DateTime
+     */
+    private $updatedAt;
 
     public function __construct()
     {
@@ -95,18 +111,6 @@ class Auteurs
         return $this;
     }
 
-    public function getPhotoAuteur(): ?string
-    {
-        return $this->photo_auteur;
-    }
-
-    public function setPhotoAuteur(?string $photo_auteur): self
-    {
-        $this->photo_auteur = $photo_auteur;
-
-        return $this;
-    }
-
     /**
      * @return Collection|Livres[]
      */
@@ -135,6 +139,53 @@ class Auteurs
             }
         }
 
+        return $this;
+    }
+
+    public function setImageFile(File $image = null)
+    {
+        $this->imageFile = $image;
+
+        // VERY IMPORTANT:
+        // It is required that at least one field changes if you are using Doctrine,
+        // otherwise the event listeners won't be called and the file is lost
+        if ($image) {
+            // if 'updatedAt' is not defined in your entity, use another property
+            $this->updatedAt = new \DateTime('now');
+        }
+    }
+
+    public function getImageFile()
+    {
+        return $this->imageFile;
+    }
+
+    public function setImage($image)
+    {
+        $this->image = $image;
+    }
+
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * @ORM\PreUpdate
+     */
+    public function updateDate()
+    {
+        $this->setUpdatedAt(new \Datetime());
+    }
+
+    public function setUpdatedAt(\Datetime $updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
         return $this;
     }
 }
